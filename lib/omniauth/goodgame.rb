90baf788d09048d01b7ca2ab505d8a8bea68d5ac
Alex Seregin <seregin_a03@mail.ru>
2023-09-07 23:32:45 +0300

    add spec

---
+++ lib/omniauth/goodgame.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "goodgame/version"
+
+module Omniauth
+  module Goodgame
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
